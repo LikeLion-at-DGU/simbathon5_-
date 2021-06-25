a2f1756c40cc7048384dae4ad15094d59af08135
shinyejin0212 <shinyejin0212@gmail.com>
2021-06-25 23:01:02 +0900

    FAQ완성

--- app1_simbathon5/models.py
+++ app1_simbathon5/models.py
@@ -5,12 +5,9 @@ from django.db import models
 class FAQ(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length= 200)
-    pub_date = models.CharField(max_length = 100)
-    body = models.DateTimeField()
+    pub_date = models.DateTimeField()
+    body = models.TextField()
 
     def __str__(self):
         return self.title
-
-    def summary(self):
-        return self.body[20]
 
