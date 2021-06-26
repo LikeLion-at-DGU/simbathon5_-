ea340ec00146a05acfbfccc1f0db98626085240a
shinyejin0212 <shinyejin0212@gmail.com>
2021-06-27 01:38:09 +0900

    찐최종

--- app1_simbathon5/admin.py
+++ app1_simbathon5/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import FAQ, Post
+from .models import Comment, FAQ, Post
 
 # Register your models here.
 
 admin.site.register(FAQ)
 admin.site.register(Post)
+admin.site.register(Comment)
